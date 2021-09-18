ddcc3c5aca762ea647ec91a63200ac15e862bfcc
TarasiukDima <dimatarasiuk91@gmail.com>
2021-09-18 15:32:28 +0300

    feat: end tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,17 +3,40 @@ import { NotImplementedError } from '../extensions/index.js';
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
- */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ *
+*/
+export default function transform(arr) {
+    if (!Array.isArray(arr)) throw Error("'arr' parameter must be an instance of the Array!");
+
+    let newArr = [...arr];
+    let operations = {
+        discard: {
+            '--discard-next': 1,
+            '--discard-prev': -1,
+        },
+        double: {
+            '--double-next': 1,
+            '--double-prev': -1,
+        },
+    }
+
+    for (let i = 0; i < newArr.length; i++) {
+        if (typeof (newArr[i]) === 'string' && newArr[i].startsWith('--')) {
+            if (newArr[i].startsWith('--discard')) {
+                newArr[i + operations.discard[newArr[i]]] = '';
+                newArr[i] = '';
+            } else if (newArr[i].startsWith('--double')) {
+                newArr[i] = newArr[i + operations.double[newArr[i]]] || '';
+            }
+        }
+    }
+    return newArr.filter(el => el !== '')
 }
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,29 +3,77 @@ import { NotImplementedError } from '../extensions/index.js';
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
- */
+ *
+*/
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(varient = true) {
+        this.varient = varient;
+        this.words = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    }
+    encrypt() {
+        if (arguments.length < 2 || typeof (arguments[0]) !== 'string') throw new Error('Incorrect arguments!');
+
+        let str = arguments[0];
+        let keyStr = arguments[1];
+        let keyStrLength = keyStr.length;
+        let newStr = '';
+        let r = 0;
+
+        for (let i in str) {
+            if (this.words.indexOf(str[i].toLowerCase()) !== -1) {
+                let indStrChar = this.words.indexOf(str[i].toLowerCase());
+                let indKeyChar = this.words.indexOf(keyStr[r % keyStrLength].toLowerCase());
+                let res = (indStrChar + indKeyChar) % 26;
+                newStr += this.words[res].toUpperCase();
+                r++;
+
+            } else {
+                newStr += str[i];
+            }
+        }
+
+        return this.varient ? newStr : newStr.split('').reverse().join('')
+    }
+
+    decrypt() {
+        if (arguments.length < 2 || typeof (arguments[0]) !== 'string') throw new Error('Incorrect arguments!');
+
+        let str = arguments[0];
+        let keyStr = arguments[1];
+        let keyStrLength = keyStr.length;
+        let newStr = '';
+        let r = 0;
+
+        for (let i in str) {
+            if (this.words.indexOf(str[i].toLowerCase()) !== -1) {
+                let indStrChar = this.words.indexOf(str[i].toLowerCase());
+                let indKeyChar = this.words.indexOf(keyStr[r % keyStrLength].toLowerCase());
+                let res = (indStrChar - indKeyChar);
+                if (res < 0) {
+                    res += 26;
+                }
+                newStr += this.words[res].toUpperCase();
+                r++;
+
+            } else {
+                newStr += str[i];
+            }
+        }
+
+        return this.varient ? newStr : newStr.split('').reverse().join('')
+    }
 }
