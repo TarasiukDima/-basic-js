b6a8fa4209838376fd168517e70e7778611483a0
TarasiukDima <dimatarasiuk91@gmail.com>
2021-09-18 15:16:12 +0300

    feat: end extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,20 +2,48 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ export default function repeater( str, options ) {
+    let newStr = str;
+    let substr = '';
+    let newOptions = {
+        repeatTimes: 1, // устанавливает количество повторений строки;
+        separator: '+', //строка, разделяющая повторы str;
+        addition: '', //это дополнительная строка, которая будет добавляться к каждому повторению строки str;
+        additionRepeatTimes: 1, //устанавливает количество повторений сложения;
+        additionSeparator: '|', //это строка, разделяющая повторы добавления
+        ...options
+    }
+
+    let {
+        repeatTimes,
+        separator,
+        addition,
+        additionRepeatTimes,
+        additionSeparator
+    } = newOptions;
+
+    while (additionRepeatTimes > 0) {
+        newStr += addition + (additionRepeatTimes > 1 ? additionSeparator : '')
+        additionRepeatTimes--;
+    }
+
+    while (repeatTimes > 0) {
+        substr += newStr + (repeatTimes > 1 ? separator : '')
+        repeatTimes--;
+    }
+
+    return substr;
 }
